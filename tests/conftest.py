b13d7387f5ad181c7b19d1db74822ca36ea37c60
Michal V <michal.varga96@gmail.com>
2026-06-23 16:39:58 -0400

    test_config.py finished

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,2 @@
 import numpy as np
 import pytest
-
-@pytest.fixture
-def simple_2d_mask():
-    """A 10x10 mask with one object (label=1) in the center"""
-    mask = np.zeros((10,10), dtype=int)
-    mask[3:7, 3:7] = 1
-    return mask
-
-@pytest.fixture
-def simple_3d_mask():
-    """A 5x10x10 mask with one object across all z-planes."""
-    mask = np.zeros((5,10,10), dtype=int)
-    mask[:, 3:7, 3:7] = 1
-    return mask
--- tests/test_assigment.py
+++ tests/test_assigment.py
@@ -2,6 +2,20 @@ import numpy as np
 import pytest
 from spot_detector.segmentation_detection import assign_spots_to_mask
 
+@pytest.fixture
+def simple_2d_mask():
+    """A 10x10 mask with one object (label=1) in the center"""
+    mask = np.zeros((10,10), dtype=int)
+    mask[3:7, 3:7] = 1
+    return mask
+
+@pytest.fixture
+def simple_3d_mask():
+    """A 5x10x10 mask with one object across all z-planes."""
+    mask = np.zeros((5,10,10), dtype=int)
+    mask[:, 3:7, 3:7] = 1
+    return mask
+
 def test_spots_inside_object(simple_2d_mask):
     coords = np.array([[5,5]])
     labels = assign_spots_to_mask(coordinates=coords, masks=simple_2d_mask)
